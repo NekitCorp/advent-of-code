a8e1f90b2e70c0961ca2a16abbe319e325c6caec
Nikita Madeev <warhammer-ss@yandex.ru>
2025-12-04 14:14:23 +0800

    Day 4.

---
+++ 2025/day-04/part-1.ts
@@ -0,0 +1,39 @@
+const input = await Bun.file(`${import.meta.dir}/input.txt`).text();
+
+const grid = input.split("\n").map((line) => line.split(""));
+
+let count = 0;
+
+const DIRS: [number, number][] = [
+    [-1, -1],
+    [0, -1],
+    [1, -1],
+    [-1, 0],
+    [1, 0],
+    [-1, 1],
+    [0, 1],
+    [1, 1],
+];
+
+for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i]!.length; j++) {
+        if (grid[i]![j] === "@") {
+            let adjacent = 0;
+
+            for (const [dx, dy] of DIRS) {
+                const nx = i + dx;
+                const ny = j + dy;
+
+                if (grid[nx]?.[ny] === "@") {
+                    adjacent += 1;
+                }
+            }
+
+            if (adjacent < 4) {
+                count += 1;
+            }
+        }
+    }
+}
+
+console.log({ count });
